b92e76394a7885d2eea70cd9510530cd15b18d0a
Mahdi Ahmadi <99219442+Mahditerorist@users.noreply.github.com>
2024-07-14 10:31:03 +0330

    Update Client.py

--- Client.py
+++ Client.py
@@ -1,38 +1,40 @@
-import requests,urllib3,json
-from typing import Literal as rangeList
+import requests
+import json
+from typing import Literal
+
 class Email:
     def __init__(
             self,
-            To:str,
-            text:str | int,
-            Title:str,
-            Token:str,#توکن دسترسی
-            Input: rangeList['info','app','Login','support'] = 'info',
+            To: str,
+            text: str | int,
+            Title: str,
+            Token: str,
+            Input: Literal['info', 'app', 'Login', 'support'] = 'info'
         ) -> None:
-        self.To=To
-        self.text=text
-        self.Title=Title
-        self.Input=Input
-        self.Token=Token
+        self.To = To
+        self.text = text
+        self.Title = Title
+        self.Input = Input
+        self.Token = Token
+    
     def Send(self):
-        Data={
-            'email':self.To,
-            'text':self.text,
-            'head':self.Title,
-            'token':self.Token,
-            'title':self.Input
+        Data = {
+            'email': self.To,
+            'text': str(self.text),  # تبدیل به رشته اطمینان می‌دهد که همیشه رشته است
+            'head': self.Title,
+            'token': self.Token,
+            'title': self.Input
         }
-        http = urllib3.PoolManager()
+        
         url = 'https://api-free.ir/api2/email.php'
-        response = http.request(
-            'POST',
-            url,
-            fields=Data
-        )
-        pars=json.loads(response.data.decode('utf-8'))
+        response = requests.post(url, data=Data)
+        pars = response.json()
         print(pars)
-        if pars['ok']:return {
-            'state':'ok',
-            'code':200
-        }
-        else:return False
+        
+        if pars.get('ok', False):
+            return {
+                'state': 'ok',
+                'code': 200
+            }
+        else:
+            return False
